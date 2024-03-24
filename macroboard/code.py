2aca379353ac7e35230b4a8cc9fb0dc3313cf26e
gamexdifficulty <gamexdifficulty@yahoo.com>
2024-03-24 16:26:19 +0100

    Added serial communication and led changing

--- macroboard/code.py
+++ macroboard/code.py
@@ -1,5 +1,6 @@
 from neopixel import NeoPixel
 from supervisor import runtime
+from rainbowio import colorwheel
 from digitalio import DigitalInOut
 from adafruit_matrixkeypad import Matrix_Keypad
 from board import C1,C2,C3,C4,R1,R2,R3,R4,NEOPIXEL
@@ -12,6 +13,12 @@ keymap = [[False,False,False,False],
           [False,False,False,False],
           [False,False,False,False]]
 
+led_mode = 2
+led_speed = 0.1
+led_brightness = 1.0
+led_breath_state = 0
+led_rainbow_index = 0.0
+
 neopixels = NeoPixel(NEOPIXEL, 4, brightness=1.0, auto_write=False)
 keypad = Matrix_Keypad(cols, rows, keys)
 
@@ -20,12 +27,25 @@ neopixels.show()
 
 while True:
 
-    # Serial Communication
+    # -------------------- Serial Communication --------------------
 
     if runtime.serial_bytes_available:
         data = input().strip()
         if data.startswith("0"):
-            print(data)
+            data = data[0:]
+
+            # Change Led
+
+            if data[0] == "0":  
+                if data[1] == "0":      # Led mode
+                    led_mode = int(data[2])
+                elif data[1] == "1":    # Led brightness
+                    led_brightness = float(data[2:])
+                elif data[1] == "2":    # Led speed
+                    led_speed = float(data[2:])
+                elif data[1] == "3":    # Led color
+                    leds = data[2:]
+                    neopixels.fill([int(leds[0:3]),int(leds[3:6]),int(leds[6:])])
 
     # Keypad event handling
 
