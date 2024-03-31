6ffccb13499a77c663c831583c55c14bb5b85a06
gamexdifficulty <gamexdifficulty@yahoo.com>
2024-04-01 01:39:43 +0200

    Added input skeleton

---
+++ app/data/classes/input.py
@@ -0,0 +1,48 @@
+import pygame
+from data.classes.text import *
+
+class Input:
+    def __init__(self,engine,pos,size,text) -> None:
+        self.engine = engine
+
+        self.pos = pos
+        self.size = size
+
+        self.percentage = 0
+
+        self.rect = pygame.Rect(pos[0],pos[1],size[0],size[1])
+        self.rect_small = pygame.Rect(self.rect.x+4,self.rect.y+4,self.rect.w-8,self.rect.h-8)
+
+        self.selected = False
+
+        self.text_id = text
+        self.text = Text(self.engine,self.text_id,pygame.Rect(self.rect.x,self.rect.y,self.rect.w-32,self.rect.h))
+
+        self.engine.inputs.append(self)
+
+    def reposition(self):
+        self.rect = pygame.Rect(self.pos[0]+self.engine.window_offset[0],self.pos[1]+self.engine.window_offset[1],self.size[0],self.size[1])
+        self.rect_small = pygame.Rect(self.rect.x+4,self.rect.y+4,self.rect.w-8,self.rect.h-8)
+        self.text.reposition()
+
+    def update(self):
+        if self.engine.input.get("released"):
+            self.selected = False
+        elif self.engine.input.get("accept"):
+            if self.rect.collidepoint(self.engine.input.mouse.get_pos()):
+                if self.engine.select_overlay != None:
+                    if not(self.engine.select_overlay.select_rect.collidepoint(self.engine.input.mouse.get_pos())):
+                        self.selected = True
+                else:
+                    self.selected = True
+
+        if self.selected:
+            pass
+
+    def draw(self):
+        if self.selected:
+            pygame.draw.rect(self.engine.window.main_surface,self.engine.color_highlight,self.rect,border_radius=8)
+            pygame.draw.rect(self.engine.window.main_surface,self.engine.color_element,self.rect_small,border_radius=4)
+        else:
+            pygame.draw.rect(self.engine.window.main_surface,self.engine.color_element,self.rect,border_radius=8)
+        self.text.draw()
